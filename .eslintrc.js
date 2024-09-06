1c6a7a871f9dd8c6a64a2737ac9ea961b6c91334
exuryio <direccion@exury.io>
2024-09-06 22:53:27 +0200

    update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 /**
- * .eslint.js
+ * .eslintrc.js
  *
  * ESLint configuration file.
  */
@@ -13,8 +13,19 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-typescript",
+    "plugin:@typescript-eslint/recommended", // Agrega esta línea para incluir las reglas recomendadas de TypeScript
+  ],
+  parserOptions: {
+    parser: "@typescript-eslint/parser", // Asegúrate de utilizar el parser correcto
+  },
+  plugins: [
+    "@typescript-eslint", // Añade el plugin de TypeScript
   ],
   rules: {
     "vue/multi-word-component-names": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn", // Puedes ajustar esto a "error" si prefieres que sea un error
+      { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }
+    ],
   },
 };
