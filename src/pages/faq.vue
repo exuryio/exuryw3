471dcb0c3f02bcb0c7e90f76c525ed8ff4246b5e
exuryio <direccion@exury.io>
2024-09-07 16:57:13 +0200

    adding copy faq

--- src/pages/faq.vue
+++ src/pages/faq.vue
@@ -18,10 +18,14 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 const items = ref([
-  {title: "1.1. ¿Cómo me registro en EXURY?", answer: "Lorem ipsum dolor sit amet consectetur. Non tellus consectetur auctor risus orci ut mollis. Tincidunt dictum ut aliquet lacus id vitae. Rutrum sit sed pretium quam condimentum elit a. Et id eleifend faucibus et mi ut amet massa vitae. Ultricies rhoncus habitasse erat vel id sapien."},
-  {title: "1.2. ¿Qué es el proceso KYC?", answer: "Lorem ipsum dolor sit amet consectetur. Non tellus consectetur auctor risus orci ut mollis. Tincidunt dictum ut aliquet lacus id vitae. Rutrum sit sed pretium quam condimentum elit a. Et id eleifend faucibus et mi ut amet massa vitae. Ultricies rhoncus habitasse erat vel id sapien."},
-  {title: "1.3. ¿Cuánto tiempo tarda la verificación?", answer: "Lorem ipsum dolor sit amet consectetur. Non tellus consectetur auctor risus orci ut mollis. Tincidunt dictum ut aliquet lacus id vitae. Rutrum sit sed pretium quam condimentum elit a. Et id eleifend faucibus et mi ut amet massa vitae. Ultricies rhoncus habitasse erat vel id sapien."},
-  {title: "2.1. ¿Cómo realizo un intercambio de criptomonedas a euros o viceversa?", answer: "Lorem ipsum dolor sit amet consectetur. Non tellus consectetur auctor risus orci ut mollis. Tincidunt dictum ut aliquet lacus id vitae. Rutrum sit sed pretium quam condimentum elit a. Et id eleifend faucibus et mi ut amet massa vitae. Ultricies rhoncus habitasse erat vel id sapien."},
+  {title: "1.1. ¿Cómo me registro en EXURY?", answer: "Regístrate en EXURY visitando nuestra web, completando el formulario y verificando tu cuenta a través del correo de confirmación."},
+  {title: "1.2. ¿De que se trata el proceso de KYC?", answer: "KYC es un proceso de verificación de identidad que garantiza la seguridad de los usuarios, cumpliendo con normativas internacionales."},
+  {title: "1.3. ¿Cuanto dura el proceso de verificación?", answer: "El proceso de verificación suele completarse en menos de 24 horas, dependiendo de la documentación"},
+  {title: "2.1. ¿Cómo realizo un intercambio de criptomonedas a euros o viceversa?", answer: "Recibiras un mensaje con la cantidad que solicites intercambiar, junto a una oferta personalizada por parte de nuestros agentes, una vez confirmada la transacción, tus fondos estarán disponibles en minutos en la wallet que nos indiques"},
+  {title: "2.2. ¿Cuáles son las tarifas de intercambio en EXURY?", answer:"Ofrecemos tarifas bajas y transparentes, que pueden variar según la criptomoneda y el volumen, al registrate nuestra asistencia personalizada te brindará todos los detalles"},
+  {title: "2.3. ¿Cómo puedo asegurar la seguridad de mis criptomonedas en EXURY?", answer: "EXURY protege tus fondos con tecnología avanzada y al ser sin custodia, tú mantienes el control total de tus claves privadas."},
+  {title: "3.1. ¿Cómo funciona el soporte al cliente en EXURY?", answer: "Nuestro soporte al cliente está disponible 24/7 a través de chat, correo electrónico y teléfono para asistencia personalizada."},
+  {title: "3.3. ¿Hay límites de intercambio en la plataforma?", answer: "Los límites dependen de la criptomoneda y tu nivel de verificación; aumenta tu verificación para ampliar tus límites de intercambio."},
 ])
 
 const onDropDownQuestion = (index: number) => {
